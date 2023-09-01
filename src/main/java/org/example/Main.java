74fd3850aa786958d7f1635752d4137f72ed7dbd
eu2goo <eu2goo@gmail.com>
2023-09-01 10:50:57 +0900

    Create Hello Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Student s1 = new Student("22200296","박소연");
+        System.out.println(s1.toString());
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Student {
+    private String name;
+    private String studentId;
+
+    public Student(String studentId) {
+        this.studentId = studentId;
+    }
+    public Student(String studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
+}
